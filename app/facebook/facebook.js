fb1465a5dabe111c6f19539f8eb99adb26183d10
sayurabh <sayurabh@gmail.com>
2018-05-04 20:13:31 +0530

    First commit

---
+++ app/facebook/facebook.js
@@ -0,0 +1,52 @@
+'use strict';
+
+angular.module('ngSocial.facebook', ['ngRoute','ngFacebook'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/facebook', {
+    templateUrl: 'facebook/facebook.html',
+    controller: 'FacebookCtrl'
+  });
+}])
+
+.config( function( $facebookProvider ) {
+  $facebookProvider.setAppId('1806716196261244');
+  $facebookProvider.setPermissions("email","public_profile", "user_posts", "publish_actions", "user_photos");
+})
+
+.run(function($rootScope){
+	(function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+})
+
+.controller('FacebookCtrl', ['$scope', '$facebook', function($scope, $facebook) {
+	$scope.isLoggedIn = false;
+	$scope.login = function(){
+		$facebook.login().then(function(){
+			$scope.isLoggedIn = true;
+      refresh();
+		});
+  }
+  $scope.logout = function(){
+    $facebook.logout().then(function(){
+      $scope.isLoggedIn = false;
+      refresh();
+    });
+  }
+    function refresh(){
+      $facebook.api("/me").then(function(response){
+        $scope.welcomeMsg = "welcome "+response.name;
+        $scope.isLoggedIn = true;
+        $scope.userInfo = response;
+      },
+      function(err){
+        $scope.welcomeMsg = "Please Log In";
+      });
+    }
+	refresh();
+}]);
